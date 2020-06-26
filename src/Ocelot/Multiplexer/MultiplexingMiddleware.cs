6e2098c0691d4caa4e88fced529d12d9be57f7b8
RayRoderic <vergilr19@hotmail.com>
2020-06-26 17:04:20 -0500

    Update MultiplexingMiddleware.cs
    the RequestAborted  property needs to be copied in target object on Copy method,  so that the request can be canceled

--- src/Ocelot/Multiplexer/MultiplexingMiddleware.cs
+++ src/Ocelot/Multiplexer/MultiplexingMiddleware.cs
@@ -184,6 +184,7 @@
             target.Request.RouteValues = source.Request.RouteValues;
             target.Connection.RemoteIpAddress = source.Connection.RemoteIpAddress;
             target.RequestServices = source.RequestServices;
+            target.RequestAborted = source.RequestAborted;
             return target;
         }
 
